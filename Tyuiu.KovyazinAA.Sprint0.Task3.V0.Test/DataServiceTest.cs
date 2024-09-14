622537a0fd6ffb551e0bcdd03f44e590bc5e04ea
KrovavyiParen <hiroazora@gmail.com>
2024-09-14 17:06:58 +0500

    Task3

---
+++ Tyuiu.KovyazinAA.Sprint0.Task3.V0.Test/DataServiceTest.cs
@@ -0,0 +1,13 @@
+using Tyuiu.KovyazinAA.Sprint0.Task3.V0.Lib;
+namespace Tyuiu.KovyazinAA.Sprint0.Task3.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(10, DataService.Sum(5, 5));
+        }
+    }
+}
